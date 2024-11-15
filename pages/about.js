ab92a0fec61d728d41cb7cf499af327b33862d8d
mickey0896 <mickey_0896@hotmail.com>
2024-11-15 17:55:22 +0700

    component style

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,13 @@
+import styles from "@/styles/About.module.css"
+import Image from "next/image"
 
 export default function About() {
     return (
         <>
-            <h1>เกี่ยวกับเรา</h1>
+            <div className={styles.container}>
+                <h1 className={styles.title}>เกี่ยวกับเรา</h1>
+                <Image src="/about.svg" width={500} height={500} alt='logo' />
+            </div>
         </>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
+import styles from '@/styles/Home.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <>
-      <h1>หน้าแรกของเว็บไซต์</h1>
+      <div className={styles.container}>
+        <h1 className={styles.title}>หน้าแรกของเว็บไซต์</h1>
+        <Image src="/shopping.svg" width={400} height={400} alt='logo' />
+        <p>ยินดีต้อนรับเข้าสู่ร้านค้า</p>
+        <Link href="/products" className={styles.btn}>ดูสินค้าทั้งหมด</Link>
+      </div>
     </>
   )
 }
