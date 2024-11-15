7a042250988b023fd8b869b8f1011a50b7c72253
mickey0896 <mickey_0896@hotmail.com>
2024-11-15 15:26:56 +0700

    layout component

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,8 @@
-import Navbar from "@/components/Navbar"
-import Footer from "@/components/Footer"
 
 export default function About() {
     return (
         <>
-            <Navbar />
             <h1>เกี่ยวกับเรา</h1>
-            <Footer />
         </>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,8 @@
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
       <h1>หน้าแรกของเว็บไซต์</h1>
-      <Footer />
     </>
-  );
+  )
 }
