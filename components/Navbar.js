e2012a15f8fd497b557b091513a1d0b9aa7a30f7
mickey0896 <mickey_0896@hotmail.com>
2024-11-15 15:12:23 +0700

    Next Link

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,14 @@
+import Link from "next/link"
+
 export default function Navbar(){
     return(
         <nav>
             <div>
                 <h1>MixShop</h1>
             </div>
-            <ul>
-                <li>หน้าแรก</li>
-                <li>เกี่ยวกับเรา</li>
-            </ul>
+            <Link href='/'>หน้าแรก</Link>
+            <Link href='/about'>เกี่ยวกับเรา</Link>
+            <Link href='products'>สินค้าทั้งหมด</Link>
         </nav>
     )
 }
